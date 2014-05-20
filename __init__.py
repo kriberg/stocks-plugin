1261974ee2175895bb2f600eeb41bef64b9d68f1
Kristian Berg <kriberg@tihlde.org>
2014-05-20 22:35:56 +0200

    Fixed project name in license

--- __init__.py
+++ __init__.py
@@ -1,19 +1,19 @@
 # Copyright (c) 2010-2013 Kristian Berg
 #
-# This file is part of EVE Corporation Management.
+# This file is part of stocks-plugin.
 #
-# EVE Corporation Management is free software: you can redistribute it and/or
+# stocks-plugin is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# EVE Corporation Management is distributed in the hope that it will be useful,
+# stocks-plugin is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
 #
 # You should have received a copy of the GNU General Public License along with
-# EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
+# stocks-plugin. If not, see <http://www.gnu.org/licenses/>.
 
 __date__ = "2013-11-09"
 __author__ = "vittoros"
